8c5d1e711a704fbfeb282ffc34368e293f42cbc8
im-Alexander <asikalevski@hotmail.com>
2016-12-17 01:04:16 -0500

    Created Levels 2 & 3

---
+++ Assets/_Scripts/RobotScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RobotScript : MonoBehaviour {
+
+    public UnityEngine.AI.NavMeshAgent Agent;
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        this.player = GameObject.FindWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.Agent.SetDestination(this.player.position);
+    }
+}
